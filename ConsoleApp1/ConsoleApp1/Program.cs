dfb183fe2ad8ccd513f07cbe84e067af83d7d04e
s26662 <s26662@pjwstk.edu.pl>
2025-03-13 04:13:45 +0100

    Utworzono metodę do obliczania średniej

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Modyfikacja 3");
+
+
+//Zadanie 3
+public static double averege(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0) return 0;
+    return numbers.Average();
+}
